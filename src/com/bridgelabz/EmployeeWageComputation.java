c5be25f4671addecb386e3799dc1e8f25a07ab6c
adityabhartib <adityabharti.ab@gmail.com>
2022-10-19 07:25:03 +0530

    Added : UC8 compute wage for multiple companies

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -8,7 +8,7 @@ public class EmployeeWageComputation {
     static final int FULLTIME = 1;
     static final int PARTTIME = 2;
     //static method for calculate empWage
-     static void computeEmpWage(int working_day_per_month,int total_working_hours,int wage_per_hour){
+     static void computeEmpWage(String CompanyName,int working_day_per_month,int total_working_hours,int wage_per_hour){
 
         int empwage = 0, workingday = 1, totalempwage = 0, workinhHrs=0;//local variable computeEmpWage Block
 
@@ -16,6 +16,7 @@ public class EmployeeWageComputation {
             double empcheck = (int) Math.floor(Math.random() * 10) % 3;
             switch ((int) empcheck) {
                 case FULLTIME:
+                    System.out.println(CompanyName+"-------");
                     System.out.println("Employee is Present");
                     empwage = wage_per_hour * FULL_DAY_HOUR;
                     System.out.println("working days: " + workingday);
@@ -24,6 +25,7 @@ public class EmployeeWageComputation {
                     workinhHrs = workinhHrs + FULL_DAY_HOUR;
                     break;
                 case PARTTIME:
+                    System.out.println(CompanyName+"--------");
                     System.out.println("Employee is Part-time");
                     empwage = wage_per_hour * PART_TIME_HOUR;
                     System.out.println("working days: " + workingday);
@@ -32,6 +34,7 @@ public class EmployeeWageComputation {
                     workinhHrs = workinhHrs + PART_TIME_HOUR;
                     break;
                 default:
+                    System.out.println(CompanyName+"-----");
                     System.out.println("Employee is Abscent");
                     System.out.println("working days: " + workingday);
                     System.out.println("EmpWage: " + empwage);
@@ -40,11 +43,12 @@ public class EmployeeWageComputation {
             workingday++;
         }
         System.out.println("--------------");
-        System.out.println("Total Working Hrs:>" + workinhHrs + "\n-------------- \nTotal Wage:>" + totalempwage);
+        System.out.println("\nCompany Name :"+CompanyName+ "\nTotal Working Hrs:>" + workinhHrs + "\n-------------- \nTotal Wage:>" + totalempwage);
     }
 
     public static void main(String[] args) {
         System.out.println("--------Welcome to Employee Wage Computation Problem--------");
-        computeEmpWage(26,150,15);
+        computeEmpWage("Reliance",26,150,15);
+        computeEmpWage("TATA",20,100,25);
     }
 }
