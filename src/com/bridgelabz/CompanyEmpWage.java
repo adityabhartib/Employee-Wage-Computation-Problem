a6b36b5fabdf0fc304f1205fe658283786ab0ea3
adityabhartib <adityabharti.ab@gmail.com>
2022-11-01 12:32:16 +0530

    Ability to Manage Multiple companies

---
+++ src/com/bridgelabz/CompanyEmpWage.java
@@ -0,0 +1,30 @@
+package com.bridgelabz;
+
+public class CompanyEmpWage {
+    public String companyName;
+    public int wagePerHr;
+    public int numWorkingDays;
+   public int maxHrsPerMonth;
+    public int totalEmpWage;
+    public CompanyEmpWage(String companyName, int wagePerHr, int numWorkingDays, int maxHrsPerMonth) {
+        this.companyName = companyName;
+        this.wagePerHr = wagePerHr;
+        this.numWorkingDays = numWorkingDays;
+        this.maxHrsPerMonth = maxHrsPerMonth;
+    }
+
+    public int getTotalEmpWage() {
+        return totalEmpWage;
+    }
+
+    public void setTotalEmpWage(int totalEmpWage) {
+        this.totalEmpWage = totalEmpWage;
+    }
+
+    @Override
+    public String toString() {
+        return "CompanyEmpWage" +
+                "Company Name :"+companyName+
+                "totalEmpWage :" + totalEmpWage;
+    }
+}
